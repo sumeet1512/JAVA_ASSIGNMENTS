59cc21aae1cd88a9301002dd7e3d4b902e2378da
sumeet1512 <109270155+sumeet1512@users.noreply.github.com>
2023-04-19 01:03:23 +0530

    Add files via upload

---
+++ Assignment5/Rectangle.java
@@ -0,0 +1,19 @@
+public class Rectangle implements Shapes {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double area() {
+        return width * height;
+    }
+
+    public double perimeter() {
+        return 2 * (width + height);
+    }
+
+}
+
